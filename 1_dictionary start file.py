a538293cab7886ebab1cf6b975ccf22d4fb53652
Mason <masoncage11@gmail.com>
2022-09-08 13:44:10 -0500

    worked on start file

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -10,16 +10,24 @@ print()
 print('*****  start section 1 - print dictionary ********')
 print()
 
+print(phonebook)
+print(type(phonebook))
+phone = phonebook['Chris']
 
+print(phone)
 
+mydictionary = {}
+print(mydictionary)
 
+mydictionary = dict(m=8, n=9)
+print(mydictionary)
 
 print()
 print('*****  end section 1 ********')
 print()
 
 
-'''
+
 
 
 print()
@@ -27,6 +35,12 @@ print('*****  start section 2 - search dictionary ********')
 print()
 
 
+name = 'Chris'
+
+if name in phonebook:
+    print(phonebook[name])
+else:
+    print(name, "not in the phonebook")
 
 
 
@@ -46,7 +60,10 @@ print()
 print('*****  start section 3 - edit/append dictionary ********')
 print()
 
-
+print(phonebook)
+phonebook['Chris']='555-0123'
+phonebook['Joe']='555-4444'
+print(phonebook)
 
 
 
@@ -63,6 +80,8 @@ print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
+#del phonebook['Chris']
+print(phonebook)
 
 
 
@@ -80,7 +99,20 @@ print('*****  start section 5 - iterate through keys, values, items ********')
 print()
 
 
+for key in phonebook:
+    print(key)
+    print(phonebook[key])
 
+for value in phonebook.values():
+    print(value)
+
+
+for k,v in phonebook.items():
+    print("key: ",k," value:",v)
+
+
+for tuple in phonebook.items():
+    print(tuple)
 
 
 
@@ -97,7 +129,11 @@ print('*****  start section 6 - using get and clear ********')
 print()
 
 
+phone = phonebook.get("Chris","Key not found")
+print(phone)
 
+
+print(phonebook)
 
 
 
@@ -105,7 +141,7 @@ print()
 print('*****  end section 6 ********')
 print()
 
-
+'''
 
 print()
 print('*****  start section 7 - using pop method ********')
